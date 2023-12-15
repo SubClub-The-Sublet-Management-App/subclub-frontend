9b31bb0895ae5e15febd32d80be1f740643274d7
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-15 19:15:13 +1100

    feat: Add nav bar responsines for small devises

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -1,25 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa';
-import { FaHome, FaUser, FaBed, FaUsers,  FaRegCalendarAlt, FaMoneyBillWave } from 'react-icons/fa';
+import { FaAngleDoubleLeft, FaBars } from 'react-icons/fa';
+import { FaHome, FaUser, FaBed, FaUsers, FaRegCalendarAlt, FaMoneyBillWave } from 'react-icons/fa';
 
 function SideNavBar() {
   const [selected, setSelected] = useState(0);
-  const [isEnlarge, setIsEnlarge] = useState(true);
+  const [isEnlarge, setIsEnlarge] = useState(window.innerWidth > 768);
 
   const nav = [
     { name: 'Dashboard', to: '/', current: true, icon: FaHome, selectedIcon: FaHome },
     { name: 'Profile', to: '/profile', current: false, icon: FaUser, selectedIcon: FaUser },
     { name: 'Rooms', to: '/rooms', current: false, icon: FaBed, selectedIcon: FaBed },
     { name: 'Occupants', to: '/occupants', current: false, icon: FaUsers, selectedIcon: FaUsers },
-    { name: 'Assignments', to: '/room-assignments', current: false, icon:  FaRegCalendarAlt, selectedIcon:  FaRegCalendarAlt },
+    { name: 'Assignments', to: '/room-assignments', current: false, icon: FaRegCalendarAlt, selectedIcon: FaRegCalendarAlt },
     { name: 'Payments', to: '/payment-records', current: false, icon: FaMoneyBillWave, selectedIcon: FaMoneyBillWave },
   ];
 
+  const checkScreenSize = () => {
+    setIsEnlarge(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-200 w-1/4">
-      <header className="pos-r h-screen inline-flex flex-col justify-between bg-white shadow p-6">
-        <nav className="inline-flex flex-col space-y-2">
+      <header className="pos-r h-screen inline-flex flex-col bg-white shadow p-6">
+        <div className="app-logo">
+          {/*  logo */}
+          <img src="/path/to/logo.png" alt="Logo" className="w-24 h-24" />
+        </div>
+        {!isEnlarge && (
+          <button
+            className="h-8 w-8 p-1 bg-gray-100 text-gray-600 rounded-lg mx-auto border border-solid border-gray-200 hover:border-gray-300"
+            onClick={() => setIsEnlarge(!isEnlarge)}
+          >
+            {isEnlarge ? <FaAngleDoubleLeft /> : <FaBars />}
+          </button>
+        )}
+        <nav className={`inline-flex flex-col space-y-2 ${isEnlarge ? 'block' : 'hidden'}`}>
             {nav.map((link, index) => (
                 <NavLink
                     key={index}
@@ -34,12 +55,6 @@ function SideNavBar() {
                 </NavLink>
             ))}
         </nav>
-        <button
-          className="h-8 w-8 p-1 bg-gray-100 text-gray-600 rounded-lg mx-auto border border-solid border-gray-200 hover:border-gray-300"
-          onClick={() => setIsEnlarge(!isEnlarge)}
-        >
-          {isEnlarge ? <FaAngleDoubleLeft /> : <FaAngleDoubleRight />}
-        </button>
       </header>
     </div>
   );
