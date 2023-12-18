f2a7af98e07c24b0e8574418d6acf76d09beb738
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-12-18 13:33:13 +1100

    feat: create Log In page

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,8 +1,54 @@
+import React from 'react';
+import logo from '../assets/images/sub-club-logo.svg'; 
+
 
 export default function LogInPage(){
     return (
-        <div>
-            <h1>This is the Log In page</h1>
+        <div className="flex flex-col sm:lg:flex-row">
+            <div className="w-full h-30vh sm:lg:h-full sm:lg:w-1/2 relative">
+                <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1582298538104-fe2e74c27f59?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWgelfHx8fGVufDB8fHx8fA%3D%3D" alt="house mates" />
+                <p className="absolute bottom-0 left-0 px-1 text-gray-300">Photo by <a href="https://unsplash.com/@heftiba?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="text-white">Toa Heftiba</a> on <a href="https://unsplash.com/photos/woman-in-black-and-white-checkered-long-sleeve-shirt-sitting-beside-man-in-green-crew-neck-l_ExpFwwOEg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="text-white">Unsplash</a></p>
+            </div>
+            <div className="signup-form w-full sm:lg:w-1/2">
+                <div className="flex max-h-1/2 min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img className="mx-auto h-10 w-auto" src={logo} alt="SubClub"/>
+                    <h2 className="mt-10 text-left text-2xl font-bold leading-9 tracking-tight text-gray-900">Log In</h2>
+                </div>
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div className="mt-2">
+                        <input id="email" name="email" type="email" autoComplete="email" required className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                        </div>
+                        <div className="mt-2">
+                        <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                        </div>
+                    </div>
+                    <div className="text-sm text-center">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                    </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                    Don't have an account yet?
+                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Sign Up for free</a>
+                    </p>
+                </div>
+                </div>
+            </div>
+
         </div>
     )
 }
